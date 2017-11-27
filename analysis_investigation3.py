ece0bce3eb2af5a9deb8e41e606a6a2c5e4f1bdd
Jordan Lui <jordan.lui@gmail.com>
2017-11-27 12:40:35 -0800

    Added relative error analysis plots

--- analysis_investigation3.py
+++ analysis_investigation3.py
@@ -23,6 +23,60 @@ def errorStats(data):
 	output = 'Error max,mean,median,min,stdev is %.2f,%.2f,%.2f,%.2f,%.2f'%(np.max(data),np.mean(data),np.median(data),np.min(data),np.std(data))
 	return output
 
+def cleanNaN(x,y):
+	newx = []
+	newy = []
+	for i,j in zip(x,y):
+		if i > 0:
+			newx.append(i)
+			newy.append(j)
+		else:
+			print 'found a NaN'
+			pass
+	x = newx
+	y = newy
+	return x,y
+
+def binPlot(x,y,bins=6):
+	# Accepts two vectors, bins the data and plots
+	# Also returns the values of the bins (x-axis)
+	if len(x) == len(y):
+		results = scipy.stats.binned_statistic(x,y,statistic='mean',bins=bins)
+
+#		N = bins
+		ind = []
+		ind2 = []
+		for i in range(len(results.bin_edges)-1):
+			ind.append(np.mean((results.bin_edges[i:i+2])))
+			ind2.append('%.0f-%.0f'%(results.bin_edges[i],results.bin_edges[i+1]))
+		ind = np.round(ind,decimals=1)
+		width = 5
+		fig, ax = plt.subplots()
+		ax.bar(ind,results.statistic,width,color='r')
+		ax.set_title('Average error at different angles %i Trials'%numTrials,fontsize=16)
+		ax.set_ylabel('Error (cm)')
+		ax.set_xlabel('Angle bin')
+		plt.show()
+		return results, ind, ind2
+	
+	
+	else:
+		print 'error! mismatch length!'
+		return
+def boxPlot(results):
+	# Box plot of data that results from a binned_statistic
+	data = []
+	for i in range(N): # Loop through bins
+		mask=results.binnumber-1 == i # Create mask
+		binvalues = np.ma.array(y,mask=~mask) # Grab the values for that mask
+		data.append(binvalues.compressed()) # Add values to a list
+	
+	fig,ax = plt.subplots()
+	ax.boxplot(np.abs(data),labels=ind2)
+	ax.set_title('Error at different angles')
+	plt.show()
+	return fig,ax
+
 #%% Parameters, setup, file loading
 path = '../Investigation3/'
 fileCoeff = 'coeff.csv'
@@ -30,7 +84,7 @@ fileRaw = 'raw.csv'
 fileScaled = 'scaled.csv'
 fileCam = 'webcam_corrected.csv'
 label = 'labels.csv'
-angles = 'angles.csv'
+angles = 'angles.csv' # Actual angles between two antenna
 
 coeff = np.genfromtxt(path+fileCoeff,delimiter=',')
 raw = np.genfromtxt(path+fileRaw,delimiter=',')
@@ -43,6 +97,7 @@ intercepts = coeff[1,:]
 
 distances = range(5,41,5) # Distance axis
 trialNo = range(1,raw.shape[1]+1)
+realDistances = np.reshape(camDistances,(camDistances.shape[0]*camDistances.shape[1],1))
 
 # Retrieve info from labels
 trials = labels[:,0]
@@ -97,9 +152,9 @@ print 'Raw: ', errorStats(np.abs(errorRaw))
 #makeHeatmap(errorRoomAvg,distances,trialNo,'Scaled with Room Average',np.max(errorRoomAvg))
 #makeHeatmap(errorRaw,distances,trialNo,'Raw Data',np.max(errorRaw))
 
-#makeHeatmap(errorScaled/camDistances,distances,trialNo,'Relative Individually Scaled',np.max(errorScaled))
-#makeHeatmap(errorRoomAvgRel,distances,trialNo,'Relative Scaled with Room Average',np.max(errorRoomAvgRel))
-#makeHeatmap(errorRaw/camDistances,distances,trialNo,'Relative Raw Data',np.max(errorRaw))
+makeHeatmap(errorScaled/camDistances,distances,trialNo,'Relative Individually Scaled',np.max(errorScaled/camDistances))
+makeHeatmap(errorRoomAvgRel,distances,trialNo,'Relative Scaled with Room Average',np.max(errorRoomAvgRel/camDistances))
+makeHeatmap(errorRaw/camDistances,distances,trialNo,'Relative Raw Data',np.max(errorRaw/camDistances))
 
 # Heat maps - Matching Scale values
 
@@ -113,7 +168,6 @@ plt.title('Error Histogram')
 #%% Examine error with distance
 errorWDistance=np.mean(np.abs(errorRoomAvg),axis=1)
 
-
 Ndistances = len(distances) # Number of distances
 ind = np.arange(Ndistances) # List of indices
 width = 0.5
@@ -126,48 +180,43 @@ ax.set_xticklabels(distances)
 ax.set_xticks(np.arange(Ndistances), minor=False)
 plt.show()
 
-#%% Error as a function of gross arm angle
-errorWTrials = np.mean(np.abs(errorRoomAvg),axis=0)
+errorWDistanceRel=np.mean(np.abs(errorRoomAvg),axis=1) / distances * 100
 
-#fig2 = plt.figure()
-#plt.scatter(angles,errorWTrials)
-#plt.title('Error With Angle')
+Ndistances = len(distances) # Number of distances
+ind = np.arange(Ndistances) # List of indices
+width = 0.5
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind,errorWDistanceRel,width,color='r')
+ax.set_title('Percent Error at different distances %i Trials'%numTrials,fontsize=16)
+ax.set_ylabel('% error, cm')
+ax.set_xlabel('Distance (cm)')
+ax.set_xticklabels(distances)
+ax.set_xticks(np.arange(Ndistances), minor=False)
+plt.show()
 
-##%% Averaged bar charts - Angles
-## Calculate the error as a function of angle
-## This code needs to be made more extensible in the future
-#
-#indAngle = []
-#for angle in angles:
-#	indAngle.append(angle/22.5)
-#angleLabels=['0','22.5','45','67.5']
-#e1=[]
-#e2=[]
-#e3=[]
-#e4=[]
-#errorWAngle=[e1,e2,e3,e4]
-#
-#for i in range(len(indAngle)): # Iterate through angles
-#	ind = int(indAngle[i]) # Grab the index value
-#	val = errorWTrials[i]  # Grab the average error for that angle
-#	errorWAngle[ind].append(val) # Sort it into the appropriate bin
-#
-#erroravgWAngle = []
-#for angle in errorWAngle:
-#	erroravgWAngle.append(np.mean(angle)) # Average error at each angle
-#
-#Ndistances = len(erroravgWAngle)
-#
-#ind = np.arange(Ndistances)
-#width = 0.5
-#fig, ax = plt.subplots()
-#rects1 = ax.bar(ind,erroravgWAngle,width,color='r')
-#ax.set_title('Error at different angles for %i Trials'%N,fontsize=16)
-#ax.set_ylabel('Average error, cm')
-#ax.set_xlabel('Angle')
-#ax.set_xticklabels(angleLabels)
-#ax.set_xticks(np.arange(Ndistances), minor=False)
-#plt.show()
+#results, ind, ind2 = binPlot(distances,errorWDistanceRel,bins=6)
+#boxPlot(results)
+
+
+
+#%% Distance  effects, relative error - INCOMPLETE
+y=[]
+if errorRoomAvg.shape[1] > 1:
+	y = np.reshape(errorRoomAvg,(errorRoomAvg.shape[0]*errorRoomAvg.shape[1],1))
+y = np.ndarray.tolist(y)
+y = [i[0] for i in y]
+
+x = np.ndarray.tolist(realDistances)
+x = [i[0] for i in x]
+#erroravgWAngleRel = np.asarray(erroravgWAngleRel)
+#y = np.ndarray.tolist(erroravgWAngleRel)
+#y = [i[0] for i in erroravgWAngleRel]
+#x = np.ndarray.tolist(realDistances)
+x,y = cleanNaN(x,y) # Remove NaN values
+bins = 8
+N = bins	
+results,ind,ind2 = binPlot(x,y,bins=bins) # Binned data, index midpoints, and index string names
+boxPlot(results) # Box plot of results
 
 #%% Effects of tag-anchor angle
 
@@ -196,65 +245,38 @@ if erroravgWAngle.shape[1] > 1:
 	y = np.reshape(erroravgWAngle,(len(angles),))
 x = np.copy(angles)
 
-def cleanNaN(x,y):
-	newx = []
-	newy = []
-	for i,j in zip(x,y):
-		if i > 0:
-			newx.append(i)
-			newy.append(j)
-		else:
-			print 'found a NaN'
-			pass
-	x = newx
-	y = newy
-	return x,y
-
-def binPlot(x,y,bins=6):
-	# Accepts two vectors, bins the data and plots
-	# Also returns the values of the bins (x-axis)
-	if len(x) == len(y):
-		results = scipy.stats.binned_statistic(x,y,statistic='mean',bins=bins)
-
-#		N = bins
-		ind = []
-		ind2 = []
-		for i in range(len(results.bin_edges)-1):
-			ind.append(np.mean((results.bin_edges[i:i+2])))
-			ind2.append('%.0f-%.0f'%(results.bin_edges[i],results.bin_edges[i+1]))
-		ind = np.round(ind,decimals=1)
-		width = 5
-		fig, ax = plt.subplots()
-		ax.bar(ind,results.statistic,width,color='r')
-		ax.set_title('Average error at different angles %i Trials'%numTrials,fontsize=16)
-		ax.set_ylabel('Error (cm)')
-		ax.set_xlabel('Angle bin')
-		plt.show()
-		return results, ind, ind2
-	
-	
-	else:
-		print 'error! mismatch length!'
-		return
-def boxPlot(results):
-	# Box plot of data
-	data = []
-	for i in range(N): # Loop through bins
-		mask=results.binnumber-1 == i # Create mask
-		binvalues = np.ma.array(y,mask=~mask) # Grab the values for that mask
-		data.append(binvalues.compressed()) # Add values to a list
-	
-	fig,ax = plt.subplots()
-	ax.boxplot(np.abs(data),labels=ind2)
-	ax.set_title('Error at different angles')
-	plt.show()
-	return
-
-x,y = cleanNaN(x,y)
+x,y = cleanNaN(x,y) # Remove NaN values
 bins = 6
 N = bins	
 results,ind,ind2 = binPlot(x,y,bins=bins) # Binned data, index midpoints, and index string names
 boxPlot(results) # Box plot of results
+
+#%% Angle effects, relative error
+#if erroravgWAngle.shape[1] > 1:
+#	y = np.reshape(erroravgWAngle,(len(angles),))
+y=[]
+if erroravgWAngle.shape[1] > 1:
+	y = np.reshape(erroravgWAngle,(len(angles),)) # Grab y values again
+erroravgWAngleRel=[]
+for i in range(len(y)):
+	erroravgWAngleRel.append(float(y[i] / realDistances[i]))
+y = erroravgWAngleRel
+#x = np.ndarray.tolist(angles)
+#x = [i[0] for i in x]
+x = angles
+#erroravgWAngleRel = np.asarray(erroravgWAngleRel)
+#y = np.ndarray.tolist(erroravgWAngleRel)
+#y = [i[0] for i in erroravgWAngleRel]
+#x = np.ndarray.tolist(realDistances)
+x,y = cleanNaN(x,y) # Remove NaN values
+bins = 6
+N = bins	
+results,ind,ind2 = binPlot(x,y,bins=bins) # Binned data, index midpoints, and index string names
+fig,ax = boxPlot(results) # Box plot of results
+#ax.set_ylabel('hi')
+#ax.set_xlabel('yes')
+#ax.set_title('Error ')
+#plt.show()
 
 #%% Manual bin method
 # Uses nice round numbers but this code is not currently extensible or easily customized
